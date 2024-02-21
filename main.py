f1b4fe667439addd552828e58fd53a46c500ea97
Jeralin <jeralinflores4@gmail.com>
2024-02-20 22:48:07 -0600

    Fisrt commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    result = suma(num1, num2)
+    print(f"La suma es: {result}")
